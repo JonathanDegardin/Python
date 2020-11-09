cee604325ccdb2bd7c0a401989087675ab25d9be
JonathanDegardin <74179678+JonathanDegardin@users.noreply.github.com>
2020-11-09 10:27:50 +0100

    Add files via upload

---
+++ Exo 5.py
@@ -0,0 +1,5 @@
+a=input("Veuillez saisir votre âge : ");
+if int(a) > 18:
+    print("Vous êtes majeur");
+else :
+    print("Vous êtes mineur");
---
+++ Exo 6.py
@@ -0,0 +1,5 @@
+x=input("Veuillez saisir un premier nombre : ");
+y=input("Veuillez saisir un second nombre : ");
+z=input("Veuillez saisir un dernier nombre : ");
+Max = max(x,y,z);
+print("Le maximum de vos trois valeurs est :",Max);
